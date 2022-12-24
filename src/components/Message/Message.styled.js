dbca7734ef106d438466744e7eac06e57f6a65ea
Kuzmich87 <d.kuzmich87@gmail.com>
2022-12-24 09:21:03 +0200

    1

---
+++ src/components/Message/Message.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  text-align: center;
+`;
+
+export const Text = styled.p`
+  font-weight: 600;
+  font-size: 25px;
+`;
